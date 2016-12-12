62feedc04af031ea308e7c877f2dd8d3867a46a0
EX master <EXmaster9@users.noreply.github.com>
2016-12-12 14:34:43 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+//     //   ) )                        
+//    //___/ /   ___     ( )  ___   /  
+//   / ___ (   //   ) ) / / //   ) /   
+//  //   | |  //   / / / / //   / /    
+// //    | | ((___( ( / / ((___/ /  
+// Code by: EliDaCoder and LXmaster
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContent("2d");
+// v is an object in which we will store our image variables
+var v = {};
+v.ground = new Image();
+v.ground.src = "ground.png"
+// drawing the ground image on the canvas; just for testing
+ctx.drawImage(v.ground,5,5);
